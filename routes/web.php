85b0dd173d7413cac3a534e467b75f2f8cc595be
4arodei <4arair@MacBook-Air-4arodei.local>
2020-04-29 13:53:36 +0300

    Complet task 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['name' => 'USER']);
+});
+
+Route::get('/articals', function () {
+    return view('articals');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
